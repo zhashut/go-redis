3bafc79407aa3d876cd6abbef3b919cd79541dd5
Zhashut <2105798885@qq.com>
2023-02-28 16:34:46 +0800

    0.2 - 实现EchoHandler

---
+++ lib/tcp/echo.go
@@ -0,0 +1,76 @@
+package tcp
+
+import (
+	"bufio"
+	"context"
+	"go-redis/lib/logger"
+	"go-redis/lib/sync/atomic"
+	"go-redis/lib/sync/wait"
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+/**
+ * Created with GoLand 2022.2.3.
+ * @author: 炸薯条
+ * Date: 2023/2/28
+ * Time: 15:55
+ * Description: No Description
+ */
+
+type EchoClient struct {
+	Conn    net.Conn
+	Waiting wait.Wait
+}
+
+func (e *EchoClient) Close() error {
+	e.Waiting.WaitWithTimeout(10 * time.Second)
+	_ = e.Conn.Close()
+	return nil
+}
+
+type EchoHandler struct {
+	activeConn sync.Map
+	closing    atomic.Boolean
+}
+
+func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
+	if handler.closing.Get() {
+		_ = conn.Close()
+		return
+	}
+	client := &EchoClient{
+		Conn: conn,
+	}
+	// 只需要key，不需要value，传struct{}
+	handler.activeConn.Store(client, struct{}{})
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				logger.Info("Connection close")
+				handler.activeConn.Delete(client)
+			}
+			logger.Warn(err)
+			return
+		}
+		client.Waiting.Add(1)
+		b := []byte(msg)
+		_, _ = conn.Write(b)
+		client.Waiting.Done()
+	}
+}
+
+func (handler *EchoHandler) Close() error {
+	logger.Info("handler shutting down...")
+	handler.closing.Set(true) // 当这里设置为true时，正在进行的handler，就会close，对应的是40行
+	handler.activeConn.Range(func(key, value any) bool {
+		client := key.(*EchoClient)
+		_ = client.Close()
+		return true
+	})
+	return nil
+}
