9b08b52e0c5ed272e6c3d87d72a474248880c6bb
Zhashut <2105798885@qq.com>
2023-03-05 20:02:28 +0800

    0.8 - 实现keys命令集

---
+++ database/keys.go
@@ -0,0 +1,120 @@
+package database
+
+import (
+	"go-redis/interface/resp"
+	"go-redis/lib/wildcard"
+	"go-redis/resp/reply"
+)
+
+/**
+ * Created with GoLand 2022.2.3.
+ * @author: 炸薯条
+ * Date: 2023/3/5
+ * Time: 19:08
+ * Description: No Description
+ */
+
+// DEL
+func execDel(db *DB, args [][]byte) resp.Reply {
+	keys := make([]string, len(args))
+	for i, arg := range args {
+		keys[i] = string(arg)
+	}
+	deleted := db.Removes(keys...)
+
+	return reply.MakeIntReply(int64(deleted))
+}
+
+// EXISTS
+func execExists(db *DB, args [][]byte) resp.Reply {
+	result := int64(0)
+	for _, arg := range args {
+		key := string(arg)
+		_, exists := db.GetEntity(key)
+		if exists {
+			result++
+		}
+	}
+
+	return reply.MakeIntReply(result)
+}
+
+// FLUSH
+func execFlush(db *DB, args [][]byte) resp.Reply {
+	db.Flush()
+	return reply.MakeOkReply()
+}
+
+// TYPE k1
+func execType(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeStatusReply("none")
+	}
+	switch entity.Data.(type) {
+	case []byte:
+		return reply.MakeStatusReply("string")
+	}
+
+	return &reply.UnknownErrReply{}
+}
+
+// RENAME K1 K2
+func execRename(db *DB, args [][]byte) resp.Reply {
+	src := string(args[0])
+	dest := string(args[1])
+
+	entity, exists := db.GetEntity(src)
+	if !exists {
+		return reply.MakeErrReply("no such key")
+	}
+	db.PutEntity(dest, entity)
+	db.Remove(src)
+
+	return reply.MakeOkReply()
+}
+
+// RENAMENX k1 k2
+func execRenameNx(db *DB, args [][]byte) resp.Reply {
+	src := string(args[0])
+	dest := string(args[1])
+
+	_, ok := db.GetEntity(dest)
+	if ok {
+		return reply.MakeIntReply(0)
+	}
+
+	entity, exists := db.GetEntity(src)
+	if !exists {
+		return reply.MakeErrReply("no such key")
+	}
+	db.PutEntity(dest, entity)
+	db.Remove(src)
+
+	return reply.MakeIntReply(1)
+}
+
+// KEYS *
+func execKeys(db *DB, args [][]byte) resp.Reply {
+	pattern := wildcard.CompilePattern(string(args[0]))
+	result := make([][]byte, 0)
+
+	db.data.ForEach(func(key string, val interface{}) bool {
+		if pattern.IsMatch(key) {
+			result = append(result, []byte(key))
+		}
+		return true
+	})
+	return reply.MakeMultiBulkReply(result)
+}
+
+func init() {
+	RegisterCommand("DEL", execDel, -2)
+	RegisterCommand("EXISTS", execExists, -2)
+	RegisterCommand("flushdb", execFlush, -1)
+	RegisterCommand("Type", execType, 2)         // TYPE k1
+	RegisterCommand("RENAME", execRename, 3)     // RENAME K1 K2
+	RegisterCommand("RENAMENX", execRenameNx, 3) // RENAMENX K1 K2
+	RegisterCommand("KEYS", execKeys, 2)         // KEYS *
+}
