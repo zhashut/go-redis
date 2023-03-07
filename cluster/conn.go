724d3b87dbcd3dfe71e30f16691ea2b8999fba25
Zhashut <2105798885@qq.com>
2023-03-07 18:10:10 +0800

    1.7 - 实现三种执行模式(1.本机节点 2.兄弟节点 3. 广播)

--- cluster/conn.go
+++ cluster/conn.go
@@ -3,7 +3,11 @@ package cluster
 import (
 	"context"
 	"errors"
+	"go-redis/interface/resp"
+	"go-redis/lib/utils"
 	"go-redis/resp/client"
+	"go-redis/resp/reply"
+	"strconv"
 )
 
 /**
@@ -39,4 +43,33 @@ func (cluster *ClusterDatabase) returnPeerClient(peer string, peerClient *client
 		return errors.New("connection not found")
 	}
 	return pool.ReturnObject(context.Background(), peerClient)
+}
+
+// 本机转发和兄弟节点转发
+func (cluster *ClusterDatabase) relay(peer string, c resp.Connection, args [][]byte) resp.Reply {
+	// 如果是自己节点，直接转发
+	if peer == cluster.self {
+		return cluster.db.Exec(c, args)
+	}
+	// 如果是兄弟节点
+	peerClient, err := cluster.getPeerClient(peer)
+	if err != nil {
+		return reply.MakeErrReply(err.Error())
+	}
+	defer func() {
+		_ = cluster.returnPeerClient(peer, peerClient)
+	}()
+	peerClient.Send(utils.ToCmdLine("SELECT", strconv.Itoa(c.GetDBIndex())))
+
+	return peerClient.Send(args)
+}
+
+// 广播转发
+func (cluster *ClusterDatabase) broadcast(c resp.Connection, args [][]byte) map[string]resp.Reply {
+	results := make(map[string]resp.Reply)
+	for _, node := range cluster.nodes {
+		result := cluster.relay(node, c, args)
+		results[node] = result
+	}
+	return results
 }
